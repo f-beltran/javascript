e857502583f99970db2bfa5b793d75123ad543e9
Franz <fbeltran@ucb.edu.bo>
2023-04-20 17:58:05 -0400

    Sintaxis

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
-let x = 10;
-let y = 20;
-let suma = x + y;
-console.log(suma)
+
+// ESTRUCTURA LEXICA
+// ---------------------------------------------------------------------- case-sensitive
+// let A = 10;
+// if ( a > 5) {
+//     console.log('Es mayor')
+// }
+// ------------------------------------------------------------------------- comentarios
+// Comentario de una linea
+/* 
+    linea 1
+    linea 2
+*/ 
+// ---------------------------------------------------------------------------- literal
+// 10;
+// 'HOLA MUNDO';
+// true;
+// false;
+// ---------------------------------------------------------------------- identificadores
+// let nombre = 'Juan';   // Valido
+// let $precio = 100.5;   // Valido
+// // let 5var = 10;      // No valido
+// let _foo = 'c'; 
