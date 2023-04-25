3247b99f2dcd244f29d267ef71ffdea292b95d32
unknown <Franz>
2023-04-25 17:34:37 -0400

    clase 25-04-2023

--- app.js
+++ app.js
@@ -85,12 +85,70 @@
 
 // ---------------------------------------------------------------- declaraciones repetidas
 
-let x = 10;
-console.log('1:', x)
-x = 'HOLA'
-console.log('2:', x)
+// let x = 10;
+// console.log('1:', x)
+// x = 'HOLA'
+// console.log('2:', x)
+// ------------------------------------------------------------------------- TIPOS DE DATOS
+// ----TIPOS PRIMITIVOS
+// ----inmutables
+// console.log(typeof(123))       // number
+// console.log(typeof("HOLA"))    // string 
+// console.log(typeof(true))      // boolean
+// console.log(typeof(undefined)) // undefined
+// console.log(typeof(null))      // object
+// -- Valores de tipo number
+// console.log(Infinity/Infinity)  // NaN
+// console.log(0/-0)               // NaN
+// console.log(-0/0)               // NaN
+// console.log(Infinity + 1)       // Infinity
+// console.log(Infinity / 1)       // Infinity
+// console.log(Infinity * 1)       // Infinity
+// console.log(Infinity * 0)       // NaN
+// -- Valores de tipo string
+// let texto = "HOLA";
+// console.log(texto.length)
+// let corazon = '❤';
+// console.log(corazon.length)
+// let feliz = '😊';
+// console.log(feliz.length)
+//-- valor null
+// ausencia de valor
+// --valos undefined
+// let mi_var;
+// console.log(mi_var)
+
+// ----TIPOS OBJETO
+// ----mutables
+let o = {
+    nombre: "Juan",
+    "apellido": "Perez",
+    'nombre completo': "Juan Perez"
+}
+function foo(){
+    // logica
+}
+
+console.log(typeof(o))          // object
+console.log(typeof( [1,2,3] ))  // object
+console.log(typeof( foo ))      // function
+
+let objeto = { x: 1 };
+let copia = objeto;
+console.log( objeto === copia )
+
+let m = { x: 1 };
+let p = { x: 1 };
+console.log( m === p )
 
 
+let u = { x: 1 };
+let v = u;
+console.log( u === v )
+console.log(u)
+console.log(v)
 
-
+u.x = 100;
+console.log(u);
+console.log(v);
 
